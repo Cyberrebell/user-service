395ce957054819a011935a50d641315446a9b758
Cyberrebell <stephan.beller@web.de>
2018-01-13 11:57:27 +0100

    improved error handling and docs

--- src/Service/Health/Healer.php
+++ src/Service/Health/Healer.php
@@ -5,32 +5,27 @@ namespace User\Service\Health;
 use ArangoDBClient\Collection;
 use ArangoDBClient\CollectionHandler;
 use ArangoDBClient\ConnectException;
-use ArangoDBClient\Exception;
 use ArangoDBClient\ServerException;
 use Psr\Log\LoggerInterface;
 
 class Healer
 {
-    const REANIMATIONS_MAX = 1;
+    const DOCUMENT_COLLECTIONS = [
+        'user',
+        'identity'
+    ];
+
+    const EDGE_COLLECTIONS = [
+        'authentication'
+    ];
 
     protected $logger;
     protected $collection;
-    protected $checks = [];
 
     public function __construct(LoggerInterface $logger, CollectionHandler $collection)
     {
         $this->logger = $logger;
         $this->collection = $collection;
-
-        $this->checks[] = function () {
-            $this->collection->get('user');
-        };
-        $this->checks[] = function () {
-            $this->collection->get('identity');
-        };
-        $this->checks[] = function () {
-            $this->collection->get('authentication');
-        };
     }
 
     public function checkHealth() : bool
@@ -44,60 +39,39 @@ class Healer
      * @param int $trysLeft
      * @return bool
      */
-    public function checkDatabaseHealth(int $trysLeft = self::REANIMATIONS_MAX) : bool
+    public function checkDatabaseHealth() : bool
     {
-        try {
-            foreach ($this->checks as $key => $checkClosure) {
-                $checkClosure();
-                unset($this->checks[$key]);
-            }
-        } catch (Exception $exception) {
-            if ($trysLeft > 0 && $this->reanimateDatabase($exception)) {
-                return $this->checkDatabaseHealth($trysLeft - 1);
-            } else {
+        $requiredCollections = array_merge(
+            self::DOCUMENT_COLLECTIONS,
+            self::EDGE_COLLECTIONS
+        );
+
+        foreach ($requiredCollections as $collectionName) {
+            try {
+                $this->collection->get($collectionName);
+            } catch (ServerException $exception) {
+                if ($exception->getMessage() === "unknown collection '" . $collectionName . "'") {
+                    $this->createMissingCollection($collectionName);
+                    $this->logger->emergency('collection "' . $collectionName . '" did not exist and was recreated!');
+                }
+            } catch (ConnectException $exception) {
+                $this->logger->emergency($exception->getMessage());
                 return false;
             }
         }
         return true;
     }
 
-    /**
-     * Try to fix a problem or log the emergency
-     *
-     * Returns false if the problem can't be fixed
-     *
-     * @param Exception $exception
-     * @return bool
-     */
-    protected function reanimateDatabase(Exception $exception) : bool
+    protected function createMissingCollection(string $collectionName)
     {
-        if ($exception instanceof ConnectException) {
-            $this->logger->emergency($exception->getMessage());
-            return false;
-        } elseif ($exception instanceof ServerException) {
-            switch ($exception->getMessage()) {
-                case 'unknown collection \'user\'':
-                    $this->logger->emergency('collection "user" did not exist and was recreated!');
-                    $collection = new Collection('user');
-                    $collection->setType(Collection::TYPE_DOCUMENT);
-                    $this->collection->create($collection);
-                    break;
-                case 'unknown collection \'identity\'':
-                    $this->logger->emergency('collection "identity" did not exist and was recreated!');
-                    $collection = new Collection('identity');
-                    $collection->setType(Collection::TYPE_DOCUMENT);
-                    $this->collection->create($collection);
-                    break;
-                case 'unknown collection \'authentication\'':
-                    $this->logger->emergency('collection "authentication" did not exist and was recreated!');
-                    $collection = new Collection('authentication');
-                    $collection->setType(Collection::TYPE_EDGE);
-                    $this->collection->create($collection);
-                    break;
-                default:
-                    break;
-            }
+        if (in_array($collectionName, self::DOCUMENT_COLLECTIONS, true)) {
+            $collection = new Collection($collectionName);
+            $collection->setType(Collection::TYPE_DOCUMENT);
+            $this->collection->create($collection);
+        } elseif (in_array($collectionName, self::EDGE_COLLECTIONS, true)) {
+            $collection = new Collection($collectionName);
+            $collection->setType(Collection::TYPE_EDGE);
+            $this->collection->create($collection);
         }
-        return true;
     }
 }
