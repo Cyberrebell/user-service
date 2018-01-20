2a123757fa4740ee18c586e5a3ab0c5df03aee1f
Cyberrebell <chainsaw75@web.de>
2018-01-20 10:40:32 +0100

    some config improvements, added schema.json files

--- src/console.php
+++ src/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use ApiMappingLayerGen\Generator\JsonSchema\JsonSchemaBuilder;
+use ApiMappingLayerGen\Generator\JsonSchema\OpenApi\JsonSchemaGenerator;
 use ApiMappingLayerGen\Generator\Php\EntityBuilder;
 use ApiMappingLayerGen\Generator\Php\EntityGenerator\Native;
 use ApiMappingLayerGen\Mapper\OpenApi\Mapper;
@@ -17,6 +19,18 @@ $console
 
         $entityBuilder = new EntityBuilder(new Native());
         $entityBuilder->buildEntities($patterns, 'User\Api', __DIR__ . '/Api');
+    })
+;
+
+$console
+    ->register('generate-json-schema')
+    ->setDescription('Rebuild the json schema files from OpenApi definition file')
+    ->setCode(function () use ($app) {
+        $mapper = new Mapper('docs/api.yml');
+        $definition = $mapper->getDefinition();
+
+        $jsonBuilder = new JsonSchemaBuilder(new JsonSchemaGenerator());
+        $jsonBuilder->buildSchemas($definition, __DIR__ . '/../docs/json');
     })
 ;
 
